8690227c926b4be6a3563d40ce77be592b75b264
MyPC <15824797314@163.com>
2020-05-06 11:01:56 +0800

    保留打卡证据

--- face_reco/apps/users/urls.py
+++ face_reco/apps/users/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    # url(r'^open_camera/$', views.OpenCamera.as_view()),  # 测定hi打开摄像头
+    url(r'^open_video/$', views.OpenVideo.as_view()),  # 打开摄像头
     url(r'^register_face/$', views.FaceCardView.as_view()),  # 识别人像
     url(r'^get_card_list/$', views.GetPersonList.as_view()),  # 获取考勤记录
     url(r'^add_user/$', views.AddUserView.as_view()),  # 增加员工
--- face_reco/settings.py
+++ face_reco/settings.py
@@ -17,6 +17,7 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, 'face_reco/apps'))
+sys.path.insert(1, os.path.join(BASE_DIR, 'face_reco/extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -27,7 +28,10 @@ SECRET_KEY = '8ww&^=c7vzklfqvaz(&vlqv54peqn8^*$_69a)1=w!vl9ybo3&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "192.168.0.188"
+]
 
 
 # Application definition
@@ -159,7 +163,57 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'front_static'),
+]
 
 
+# 把静态文件收集到 STATIC_ROOT中 python manage.py collectstatic
+# 资源部署
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'front_static/static')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  # 日志信息显示的格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'filters': {  # 对日志进行过滤
+        'require_debug_true': {  # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  # 日志处理方法
+        'console': {  # 向终端中输出日志
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), "face_reco/face_reco/logs/face.log"),  # 日志文件的位置
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {  # 日志器
+        'django': {  # 定义了一个名为django的日志器
+            'handlers': ['console', 'file'],  # 可以同时向终端与文件中输出日志
+            'propagate': True,  # 是否继续传递日志信息
+            'level': 'INFO',  # 日志器接收的最低日志级别
+        },
+    }
+}
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+
+# MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media').replace("\\", "/")
 
